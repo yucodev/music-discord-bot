03178f39c348237939de66cff2ec35f87928f4de
viktaur <30535579+viktaur@users.noreply.github.com>
2020-09-11 12:42:41 +0200

    hello update

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ async def on_message(message):
         return
     if message.content.startswith('Hola'):
         await message.channel.send('Holaaa!')
+	if message.content.startswith('Hello'):
+		await message.channel.send('Helooo!')
 
 @bot.command(pass_context = True)
 async def conectar(ctx):
     canal = ctx.message.author.voice.channnel
     if not canal:
-        await ctx.send('No estas conectado a un canal')
+        await ctx.send('You are not connected to any channel')
         return
     voz = get(bot.voice_clients,guild=ctx.guild)
     if voz and voz.is_connected():
